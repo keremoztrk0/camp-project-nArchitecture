1e70ccec62cadcc58163274d7c02b198316f8202
keremoztrk0 <keremozturk26@gmail.com>
2022-08-23 19:52:08 +0300

    Day 2 CQRS,Business Rules & Global Exception implemented

---
+++ src/demoProjects/rentACar/Application/Features/Brands/Rules/BrandBusinessRules.cs
@@ -0,0 +1,25 @@
+﻿using Application.Services.Repositories;
+using Core.CrossCuttingConcerns.Exceptions;
+using Core.Persistence.Paging;
+using Domain.Entities;
+using System;
+using System.Linq;
+
+namespace Application.Features.Brands.Rules
+{
+    public class BrandBusinessRules
+    {
+        private readonly IBrandRepository _brandRepository;
+
+        public BrandBusinessRules(IBrandRepository BrandRepository)
+        {
+            _brandRepository = BrandRepository;
+        }
+
+        public async Task BrandNameCanNotBeDuplicatedWhenInserted(string name)
+        {
+            IPaginate<Brand> result = await _brandRepository.GetListAsync(b => b.Name == name);
+            if (result.Items.Any()) throw new BusinessException("Brand name exists.");
+        }
+    }
+}
